114b5b958e96488ed9f49cd22d6abbe64253a07c
DerCodenascher <49376631+DerCodenascher@users.noreply.github.com>
2019-04-23 09:44:23 +0200

    Revert "xxx"
    This reverts commit 7725c1bd720c27f1e3fd7f204150a5c851d97458.

--- src/Stock.java
+++ src/Stock.java
@@ -3,25 +3,23 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class Stock {
-
-    private String[] latestHistory = null;
-    private String wkn;
-    private String symbol;
-    private String name;
-
     // constructor method
-    Stock(String name, String symbol, String number) { // , String wkn, String token
+    Stock(String name, String symbol, String number){ // , String wkn, String token
         this.name = name;
         this.wkn = number; // Wertpapierkennnummer
         this.symbol = symbol; // Kürzel
     }
+    private String name;
+    private String[] latestHistory = null;
+    private String wkn;
+    private String symbol;
 
-    private void showCurrentHistory() {
+    private void showCurrentHistory(){
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date date = new Date();
         System.out.println("LATEST HISTORY " + dateFormat.format(date)); //2016/11/16 12:08:43
 
-        if (latestHistory != null) {
+        if( latestHistory != null){
             System.out.format("===================================================================================================\n");
             System.out.format("| %-11s | %-11s | %-11s | %-11s | %-11s | %-11s | %-11s |\n", "Date", "Open", "High", "Low", "Close", "Volume", "ADJ Close" );
             System.out.format("===================================================================================================\n|");
