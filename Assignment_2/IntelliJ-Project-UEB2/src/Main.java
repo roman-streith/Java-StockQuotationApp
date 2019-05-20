5075af3b1398572d988f17e03043a351c7e35146
DerCodenascher <49376631+DerCodenascher@users.noreply.github.com>
2019-05-20 14:32:01 +0200

    added "treecheck filename" syntax functionality

--- Assignment_2/IntelliJ-Project-UEB2/src/Main.java
+++ Assignment_2/IntelliJ-Project-UEB2/src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();  //create emtpy tree object
-        FileManager.fileToTree("avl", bst);     //import values from file
-        bst.checkAVL(bst.getRoot());    //check if tree is avl, calculate node balance values
-        bst.printStatistics();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            BinarySearchTree bst = new BinarySearchTree();  //create emtpy tree object
+            FileManager.fileToTree(input.split(" ")[1], bst);     //import values from file
+            bst.checkAVL(bst.getRoot());    //check if tree is avl, calculate node balance values
+            bst.printStatistics();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
